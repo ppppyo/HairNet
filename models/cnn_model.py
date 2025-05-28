cf8019c68ea8c57f3c97aaad9e0e3ea3f985ebd1
ppppyo <132552091+ppppyo@users.noreply.github.com>
2025-05-28 13:22:37 +0900

    new_model

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -5,27 +5,21 @@ class CNNModel(nn.Module):
         super(CNNModel, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 32, 3, padding=1), nn.BatchNorm2d(32), nn.ReLU(),               # → 256 x 256
-            nn.Conv2d(32, 32, 3, padding=1), nn.BatchNorm2d(32), nn.ReLU(),              # → 256 x 256
+            nn.Conv2d(32, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU(),              # → 256 x 256
             nn.MaxPool2d(2),                                         # → 128 x 128
 
-            nn.Conv2d(32, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU(),              # → 128 x 128
-            nn.Conv2d(64, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU(),              # → 128 x 128
+            nn.Conv2d(64, 128, 3, padding=1), nn.BatchNorm2d(128), nn.ReLU(),
+            nn.Conv2d(128, 128, 3, padding=1), nn.BatchNorm2d(128), nn.ReLU(),
             nn.MaxPool2d(2),
-                                                     
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2),  # 32x32         
-            
-            nn.AdaptiveAvgPool2d((1, 1))                                                                         # → 64 x 64
+
+            nn.Conv2d(128, 256, 3, padding=1), nn.BatchNorm2d(256), nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1))                                                                        # → 64 x 64
         )
 
         self.classifier = nn.Sequential(
-            nn.Flatten(),             # (64,1,1) → (64,)
-            nn.Linear(64, 128),
-            nn.ReLU(),
+            nn.Flatten(),             # (64,1,1) → (64,),
             nn.Dropout(0.3),
-            nn.Linear(128, num_classes)
+            nn.Linear(256, num_classes)
         )
 
     def forward(self, x):
